d4b17ef4fed9f244a5d451f590ff8f684d068705
Nathan Longbotham <nlongbotham@digitalglobe.com>
2016-06-06 19:10:00 +0000

    Add block_view function to base.

--- geoio/base.py
+++ geoio/base.py
@@ -1506,3 +1506,14 @@ class GeoSet(Sequence):
     def get_data(self):
 
         pass
+
+from numpy.lib.stride_tricks import as_strided
+def block_view(A, block=(3,3), strides=(2,2)):
+        """Provide a 2D block view to 2D array. No error checking made.
+        Therefore meaningful (as implemented) only for blocks strictly
+        compatible with the shape of A."""
+        # simple shape and strides computations may seem at first strange
+        # unless one is able to recognize the 'tuple additions' involved ;-)
+        shape= (A.shape[0]/ block[0], A.shape[1]/ block[1])+ block
+        strides= (strides[0]* A.strides[0], strides[1]* A.strides[1])+ A.strides
+        return as_strided(A, shape= shape, strides= strides)
